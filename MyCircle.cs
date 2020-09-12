4bbcf2181e6705ed9c0afeab9952b7f8c328fea3
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2020-09-12 11:18:04 +0300

    #MyCircle: закончен метод обрезки

--- MyCircle.cs
+++ MyCircle.cs
@@ -7,17 +7,27 @@ using System.Threading.Tasks;
 
 namespace OOP_Paint {
     class MyCircle : Figure {
-        public override void Draw(Point _upLeft, Point _downRight, Pen _pen, Graphics _screen) {
-            int radius = FindRadius(_upLeft, _downRight);
+        public override void Draw(Point _firstClick, Point _secondClick, Pen _pen, Graphics _screen) {
+            CutCoordinatesRectangleToSquare(_firstClick, _secondClick);
+            int radius = FindRadius(_firstClick, _secondClick);
         }
-        private int FindRadius(Point _upLeft, Point _downRight) {
-            var square = CutCoordinatesRectangleToSquare(Point _upLeft, Point _downRight);
+        private int FindRadius(Point _p1, Point _p2) {
+            var square = CutCoordinatesRectangleToSquare(Point _p1, Point _p2);
         }
-        //???Метод принимает верхняя точка всегда слева, а рисование возможно и снизу
-        //Либо менять местами, либо как-то конвертировать
-        private (Point, Point) CutCoordinatesRectangleToSquare(Point _upLeft, Point _downRight) {
-            switch(Math.Sign(_downRight.X - _upLeft.X - (_downRight.Y - _upLeft.Y)) {
 
+        /// <summary>
+        /// Обрубает координатный прямоугольник до квадрата
+        /// </summary>
+        /// <param name="_p1"></param>
+        /// <param name="_p2"></param>
+        private void CutCoordinatesRectangleToSquare(Point _p1, Point _p2) {
+            int a = _p2.X - _p1.X;
+            int b = _p2.Y - _p1.Y;
+            if (Math.Abs(a) > Math.Abs(b)) {
+                _p2.X = _p1.X + b;
+            }
+            else {
+                _p2.Y = _p1.Y + a;
             }
         }
     }
